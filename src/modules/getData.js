fffd9180644a6b46a350ef02b21d3045a24b9111
Sergey <serzh.kaminskiy.00@inbox.ru>
2025-11-18 03:49:42 +0300

    add firebase server

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,5 @@
 const getData = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users/')
+    return fetch('https://test-e4ca5-default-rtdb.firebaseio.com/goods.json')
         .then((response) => {
             return response.json()
         })
