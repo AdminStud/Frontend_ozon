c57755c171e6608b35089160574f7eb4265a7f0d
Sergey <serzh.kaminskiy.00@inbox.ru>
2025-11-24 16:46:15 +0300

    feat: Добавлена функция удаления из корзины

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -2,27 +2,33 @@ import renderCart from "./renderСart"
 
 const cart = () => {
 
-    const cartBtn = document.getElementById("cart")
-    const cartModal = document.querySelector(".cart")
-    const cartCloseBtn = cartModal.querySelector(".cart-close")
+    const cartBtn = document.getElementById('cart')
+    const cartModal = document.querySelector('.cart')
+    const cartCloseBtn = cartModal.querySelector('.cart-close')
+    const cartTotal = cartModal.querySelector('.cart-total > span')
     const goodsWrapper= document.querySelector('.goods')
-    // cart-total
+    const cartWrapper = document.querySelector('.cart-wrapper')
+
+
     const openCart = () => {
         const cart = localStorage.getItem('cart') ? 
                  JSON.parse(localStorage.getItem('cart')) : []
-        cartModal.style.display = "flex"
+        cartModal.style.display = 'flex'
 
         renderCart(cart)
+        cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price
+        }, 0)
     }
 
     const closeCart = () => {
-        cartModal.style.display = ""
+        cartModal.style.display = ''
     }
 
-    cartBtn.addEventListener("click", openCart)
-    cartCloseBtn.addEventListener("click", closeCart)
+    cartBtn.addEventListener('click', openCart)
+    cartCloseBtn.addEventListener('click', closeCart)
 
-    goodsWrapper.addEventListener("click", (event) => {
+    goodsWrapper.addEventListener('click', (event) => {
         if (event.target.classList.contains('btn-primary')) {
             const card = event.target.closest('.card')
             const key = card.dataset.key
@@ -41,6 +47,28 @@ const cart = () => {
         }
     })
 
+    cartWrapper.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn-primary')){
+            const cart = localStorage.getItem('cart') ? 
+                 JSON.parse(localStorage.getItem('cart')) : []
+            const card = event.target.closest('.card')
+            const key = card.dataset.key
+
+            const index = cart.findIndex((item) => {
+                return item.id === key
+            })
+            
+            cart.splice(index, 1)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            renderCart(cart)
+
+            cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price
+        }, 0)
+        }
+    })
 }
 
 export default cart
