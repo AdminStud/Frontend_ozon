acaaba46797068ab1262d36d28e49972e901bf64
Sergey <serzh.kaminskiy.00@inbox.ru>
2025-11-24 13:58:04 +0300

    feat: Добавлен Json-массив cart

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -20,12 +20,17 @@ const cart = () => {
         if (event.target.classList.contains('btn-primary')) {
             const card = event.target.closest('.card')
             const key = card.dataset.key
+
             const goods = JSON.parse(localStorage.getItem('goods'))
-            const cart = localStorage.getItem('cart') ? 
-                 JSON.parse(localStorage.getItem('goods')) : []
-            
-            console.log(goods);
-            
+
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+            const goodItem = goods.find((item) => {
+                return item.id === key
+            })
+
+            cart.push(goodItem)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
         }
     })
 
